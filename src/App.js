5eeee34ebc11bb42ad7a50a85f6ea53eb5ab247f
a5757657 <95263890+a5757657@users.noreply.github.com>
2023-03-15 10:35:11 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import ScratchCard from 'react-scratchcard-v2';
+import img from './asset/images/ckeditor-63b3e68ce1a15.jpg'
+import img2 from './asset/images/788debc0-7525-11ed-bfcf-a89c3ac8d01f.jpeg'
+import { Fireworks } from '@fireworks-js/react'
+import './App.css'
+import './styles/ScratchCard/ScratchCard.scss'
 
 function App() {
+  const ref = useRef(null);
+  const fireworkRef = useRef(null);
+  const onClickReset = () => {
+    ref.current && ref.current.reset();
+    fireworkRef.current.stop()
+  }
+  const handleFireworkStart = () => {
+    fireworkRef.current.start()
+    setTimeout(() => {
+      fireworkRef.current.stop()
+      clearTimeout()
+    }, 5000);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScratchCard
+        width={300}
+        height={250}
+        finishPercent={60}
+        image={img}
+        ref={ref}
+        onComplete={handleFireworkStart}
+        brushSize={30}
+      >
+        <img
+          alt=''
+          src={img2}
+          style={{
+            display: 'flex',
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            objectFit: 'cover',
+          }}
+        />
+      </ScratchCard>
+      <Fireworks
+        ref={fireworkRef}
+        autostart={false}
+        options={{
+
+        }}
+        style={{
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          position: 'fixed',
+          zIndex: 99,
+          pointerEvents: 'none',
+        }}
+      />
+      <button onClick={onClickReset}>Reset</button>
     </div>
   );
 }
