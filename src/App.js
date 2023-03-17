e89d59e14443b13470bbca30bb1a05fe8c0ed527
a5757657 <95263890+a5757657@users.noreply.github.com>
2023-03-17 15:45:49 +0800

    add scratch card image

--- src/App.js
+++ src/App.js
@@ -1,66 +1,10 @@
-import React, { useRef } from 'react';
-import ScratchCard from 'react-scratchcard-v2';
-import img from './asset/images/ckeditor-63b3e68ce1a15.jpg'
-import img2 from './asset/images/788debc0-7525-11ed-bfcf-a89c3ac8d01f.jpeg'
-import { Fireworks } from '@fireworks-js/react'
+import React from 'react';
+import ScratchCardContainer from './pages/ScratchCardContainer';
 import './App.css'
-import './styles/ScratchCard/ScratchCard.scss'
-
 function App() {
-  const ref = useRef(null);
-  const fireworkRef = useRef(null);
-  const onClickReset = () => {
-    ref.current && ref.current.reset();
-    fireworkRef.current.stop()
-  }
-  const handleFireworkStart = () => {
-    fireworkRef.current.start()
-    setTimeout(() => {
-      fireworkRef.current.stop()
-      clearTimeout()
-    }, 5000);
-  }
   return (
     <div className="App">
-      <ScratchCard
-        width={300}
-        height={250}
-        finishPercent={60}
-        image={img}
-        ref={ref}
-        onComplete={handleFireworkStart}
-        brushSize={30}
-      >
-        <img
-          alt=''
-          src={img2}
-          style={{
-            display: 'flex',
-            width: '100%',
-            height: '100%',
-            alignItems: 'center',
-            justifyContent: 'center',
-            objectFit: 'cover',
-          }}
-        />
-      </ScratchCard>
-      <Fireworks
-        ref={fireworkRef}
-        autostart={false}
-        options={{
-
-        }}
-        style={{
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          position: 'fixed',
-          zIndex: 99,
-          pointerEvents: 'none',
-        }}
-      />
-      <button onClick={onClickReset}>Reset</button>
+      <ScratchCardContainer/>
     </div>
   );
 }
---
+++ src/pages/ScratchCardContainer.js
@@ -0,0 +1,98 @@
+import React, { useRef, useEffect, useState } from 'react';
+import ScratchCard from 'react-scratchcard-v2';
+import { Fireworks } from '@fireworks-js/react'
+import img from '.././asset/images/scratch-card/scratch-section.png'
+import benz from '.././asset/images/scratch-card/benz.jpg'
+import logo from '.././asset/images/scratch-card/logo.png'
+import '.././styles/ScratchCardContainer.scss'
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+
+const ScratchCardContainer = () => {
+  const ref = useRef(null);
+  const fireworkRef = useRef(null);
+  const [height, setHeight] = useState('fit-content')
+
+  const onClickReset = () => {
+    ref.current && ref.current.reset();
+    fireworkRef.current.stop()
+  }
+
+  const handleFireworkStart = () => {
+    fireworkRef.current.start()
+    setTimeout(() => {
+      fireworkRef.current.stop()
+      clearTimeout()
+    }, 5000);
+  }
+
+  const setLogoHeight = () => {
+    if (window.innerWidth > window.innerHeight) {
+      setHeight(window.innerHeight - 130)
+    } else {
+      setHeight('fit-content')
+    }
+  }
+
+  useEffect(() => {
+    AOS.init()
+    setLogoHeight()
+    window.addEventListener('resize', setLogoHeight)
+    return () => {
+      window.removeEventListener('resize', setLogoHeight)
+    }
+  }, [])
+  
+  return (
+    <div className="container" onDoubleClick={onClickReset}>
+      <div data-aos="fade-in" data-aos-delay="300" data-aos-duration="1000" className="top" style={{ height: height, marginTop: height === 'fit-content' ? -50 : -80 }}>
+          <img src={logo} alt=""/>
+      </div>
+      <div data-aos="fade-in" data-aos-delay="600" data-aos-duration="1000" className="bottom">
+        <div className="bg">
+          <ScratchCard
+            width={300}
+            height={80}
+            finishPercent={50}
+            image={img}
+            ref={ref}
+            onComplete={handleFireworkStart}
+            brushSize={20}
+          >
+            <img
+              alt=''
+              src={benz}
+              style={{
+                display: 'flex',
+                width: '100%',
+                height: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                objectFit: 'cover',
+              }}
+            />
+          </ScratchCard>
+        </div>
+        <div className="border"></div>
+      </div>
+      <Fireworks
+        ref={fireworkRef}
+        autostart={false}
+        options={{
+
+        }}
+        style={{
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          position: 'fixed',
+          zIndex: 99,
+          pointerEvents: 'none',
+        }}
+      />
+    </div>
+  )
+}
+
+export default ScratchCardContainer
